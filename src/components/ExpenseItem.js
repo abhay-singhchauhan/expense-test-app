9e817b0852e51eaf56d263e6e93ed948fca80073
abhay singh chauhan <itsyourabhay@gmail.com>
2023-12-04 22:48:33 +0530

    added the first commit of the project

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,17 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <h2>Expense Item's</h2>
+      <div>
+        <div>Food</div> <div>Rs 10</div>
+      </div>{" "}
+      <div>
+        <div>Petrol</div> <div>Rs 100</div>
+      </div>
+      <div>
+        <div>Movies</div> <div>Rs 200</div>
+      </div>
+    </div>
+  );
+}
+export default ExpenseItem;
