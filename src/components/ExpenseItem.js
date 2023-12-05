7503f195c1d8a0c648692e15a350ae2acc3dd609
abhay singh chauhan <itsyourabhay@gmail.com>
2023-12-05 17:43:20 +0530

    element will get removed when we click to the button

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,15 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  const buttonClickHandler = (e) => {
+    e.target.parentElement.remove();
+  };
+
   return (
     <div className="mainDiv">
       <div>{props.Title}</div> <div>Rs {props.Ammount}</div>
       <div> {props.Location}</div>
+      <button onClick={buttonClickHandler}>Change Title</button>
     </div>
   );
 }
