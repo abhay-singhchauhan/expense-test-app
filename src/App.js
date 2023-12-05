b48ee360252d01b29f5d61482f607bcb7a9feb35
abhay singh chauhan <itsyourabhay@gmail.com>
2023-12-05 15:25:31 +0530

    added props

--- src/App.js
+++ src/App.js
@@ -4,7 +4,17 @@ function App() {
   return (
     <div>
       <h2>Let's get started achas beetetdifh apdsfjadsfhaf;hdsakjhs;d!</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem
+        Title="Abhay"
+        Ammount="800000"
+        Location="Delhi"
+      ></ExpenseItem>
+      <ExpenseItem Title="Shahid" Ammount="50" Location="America"></ExpenseItem>
+      <ExpenseItem
+        Title="Numan"
+        Ammount="5000"
+        Location="New York"
+      ></ExpenseItem>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,24 +1,10 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
-  const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 546.25;
-  const locationOfExpenditure = "Delhi";
-
+function ExpenseItem(props) {
   return (
     <div className="mainDiv">
-      <h2>Expense Item's</h2>
-      <div>
-        <div>{expenseTitle}</div> <div>Rs {expenseAmount}</div>
-        <div> {locationOfExpenditure}</div>
-      </div>
-      <div>
-        <div>Petrol</div> <div>Rs 100</div>
-      </div>
-      <div>
-        <div>Movies</div> <div>Rs 200</div>
-      </div>
+      <div>{props.Title}</div> <div>Rs {props.Ammount}</div>
+      <div> {props.Location}</div>
     </div>
   );
 }
