a298049167aac0e298f209c668a920b946300536
abhay singh chauhan <itsyourabhay@gmail.com>
2023-12-06 16:04:19 +0530

    learned useState

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,24 @@
 import "./ExpenseItem.css";
+import { useState } from "react";
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.Title);
+  const [ammount, setAmmount] = useState(props.Ammount);
   const buttonClickHandler = (e) => {
-    e.target.parentElement.remove();
+    setTitle("Hmm");
+    console.log(title);
+  };
+  const changeAmmount = () => {
+    setAmmount("100");
+    console.log(ammount);
   };
 
   return (
     <div className="mainDiv">
-      <div>{props.Title}</div> <div>Rs {props.Ammount}</div>
+      <div>{title}</div> <div>Rs {ammount}</div>
       <div> {props.Location}</div>
       <button onClick={buttonClickHandler}>Change Title</button>
+      <button onClick={changeAmmount}>Change Ammount</button>
     </div>
   );
 }
